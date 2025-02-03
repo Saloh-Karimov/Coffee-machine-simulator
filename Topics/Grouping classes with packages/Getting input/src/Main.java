fbb53218744590ff6268cb6f7f3b627e1806000a
Saloh <karimov1994@gmail.com>
2025-02-03 17:15:55 +0500

    Remove all files

--- Topics/Grouping classes with packages/Getting input/src/Main.java
+++
@@ -1,7 +0,0 @@
-class InputReader {
-    public static String getString() {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        String input = scanner.nextLine();
-        return input;
-    }
-}
--- Topics/Increment and decrement/Decrement all/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int z = 'z';
-        char mystery = (char)(z - 10);
-        System.out.println(mystery);
-    }
-}
